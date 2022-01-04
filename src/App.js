4ed61b13281f32b3ef1510cb7778ebb41be75176
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2022-01-04 14:16:32 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+ import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = new Set(items.map((item)=> item.category));
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(['all',...allCategories]);
+  console.log(categories);
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    else {
+      const newItems= items.filter((item)=> item.category === category);
+      setMenuItems(newItems);
+    }
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories}/>
+        <Menu items ={menuItems}/>
+      </section>
+    </main>
+  )
+}
+
+export default App;
